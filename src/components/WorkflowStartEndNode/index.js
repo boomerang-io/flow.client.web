1e547489681153890d27fca046710ee2c8369148
TIMOTHY R. BULA <trbula@us.ibm.com>
2018-10-23 10:37:43 +0300

    style: update node style

--- src/components/WorkflowStartEndNode/index.js
+++ src/components/WorkflowStartEndNode/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { PortWidget } from "@boomerang/boomerang-dag";
-import { Tile } from "carbon-components-react";
 import classnames from "classnames";
 import "./styles.scss";
 
@@ -13,12 +12,12 @@ class StartEndNodeWidget extends Component {
     //console.log("startEnd props");
     //console.log(this.props);
     return (
-      <div className="b-StartEndNode">
-        <Tile className="startend__tile"> {this.props.node.passedName} </Tile>
+      <div className="b-startEnd-node">
+        <div className="b-startEnd-node__title"> {this.props.node.passedName} </div>
         {this.props.node.passedName === "Finish" ? (
-          <PortWidget className={classnames("b-StartEndNode-port", "--left")} name="left" node={this.props.node} />
+          <PortWidget className={classnames("b-startEnd-node__port", "--left")} name="left" node={this.props.node} />
         ) : (
-          <PortWidget className={classnames("b-StartEndNode-port", "--right")} name="right" node={this.props.node} />
+          <PortWidget className={classnames("b-startEnd-node__port", "--right")} name="right" node={this.props.node} />
         )}
       </div>
     );
