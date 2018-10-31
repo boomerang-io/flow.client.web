723a89ebf5331cdafc497547731238353e72449f
TIMOTHY R. BULA <trbula@us.ibm.com>
2018-10-31 14:09:47 +0200

    fix: activity diagram fixes

--- src/features/WorkflowActivity/index.js
+++ src/features/WorkflowActivity/index.js
@@ -14,7 +14,7 @@ class WorkflowActivityContainer extends Component {
 
   constructor(props) {
     super(props);
-    this.diagramApp = new DiagramApplication(props.workflow, true);
+    this.diagramApp = new DiagramApplication({ dag: props.workflow.dag, isLocked: true });
   }
 
   render() {
