7b1eac5da90e9809fb92037d62fdcfc5c3375b6d
TIMOTHY R. BULA <trbula@us.ibm.com>
2018-10-31 17:17:20 +0200

    fix: text for action button

--- src/features/WorkflowManager/Creator/index.js
+++ src/features/WorkflowManager/Creator/index.js
@@ -82,7 +82,7 @@ class WorkflowCreatorContainer extends Component {
             render={props => (
               <>
                 <ActionBar
-                  actionButtonText={this.state.hasCreatedWorkflow ? "Update" : "Create"}
+                  actionButtonText={this.state.hasCreatedWorkflow ? "Update" : "Create New Version"}
                   onClick={this.designerAction}
                   diagramApp={this.diagramApp}
                   includeZoom
