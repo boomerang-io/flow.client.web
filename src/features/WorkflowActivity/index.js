4babe95a4ab684fe5d49383150555dea1f616ff9
Benjamin Ruby <Benjamin.Ruby@ibm.com@Benjamins-MacBook-Pro.local>
2018-11-01 22:03:20 -0500

    feat: workflow re-rendered on activity screen

--- src/features/WorkflowActivity/index.js
+++ src/features/WorkflowActivity/index.js
@@ -1,49 +1,63 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 import DiagramApplication from "Utilities/DiagramApplication";
 import { DiagramWidget } from "@boomerang/boomerang-dag";
+import { actions as workflowActions } from "State/workflow/fetch";
+import { BASE_SERVICE_URL, REQUEST_STATUSES } from "Config/servicesConfig";
 
 class WorkflowActivityContainer extends Component {
   static propTypes = {
-    tasks: PropTypes.object.isRequired,
-    tasksActions: PropTypes.object.isRequired,
     workflow: PropTypes.object,
-    workflowConfigActions: PropTypes.object.isRequired
+    workflowActions: PropTypes.object.isRequired
   };
+
+  componentDidMount() {
+    const { match } = this.props;
+    const { workflowId } = match.params;
+    this.props.workflowActions.fetch(`${BASE_SERVICE_URL}/workflow/${workflowId}`);
+  }
 
   constructor(props) {
     super(props);
-    this.diagramApp = new DiagramApplication(props.workflow, true);
+    //this.diagramApp = new DiagramApplication(props.workflow, true);
   }
 
   render() {
-    return (
-      <div className="content">
-        <div className="diagram-layer">
-          <DiagramWidget
-            className="srd-demo-canvas"
-            diagramEngine={this.diagramApp.getDiagramEngine()}
-            maxNumberPointsPerLink={0}
-            smartRouting={true}
-            deleteKeys={[]}
-            allowLooseLinks={false}
-            allowCanvasTranslation={false}
-            allowCanvasZoo={false}
-          />
+    const { workflow } = this.props;
+
+    if (workflow.status === REQUEST_STATUSES.SUCCESS) {
+      this.diagramApp = new DiagramApplication(workflow.data, true);
+      return (
+        <div className="content">
+          <div className="diagram-layer">
+            <DiagramWidget
+              className="srd-demo-canvas"
+              diagramEngine={this.diagramApp.getDiagramEngine()}
+              maxNumberPointsPerLink={0}
+              smartRouting={true}
+              deleteKeys={[]}
+              allowLooseLinks={false}
+              allowCanvasTranslation={false}
+              allowCanvasZoo={false}
+            />
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
 
     return null;
   }
 }
 
 const mapStateToProps = (state, ownProps) => ({
-  workflow: state.workflow.fetch.data.find(workflow => workflow.id === ownProps.match.params.workflowId)
+  workflow: state.workflow.fetch
 });
 
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => ({
+  workflowActions: bindActionCreators(workflowActions, dispatch)
+});
 
 export default connect(
   mapStateToProps,
