fa515448d1238bdd6fef777ffec34f23104bc1cb
Isabela Mayumi Kawabata Borges <isabela.kawabata@ibm.com>
2018-11-12 17:50:07 -0200

    fix: updated change log service call

--- src/features/WorkflowManager/components/ChangeLog/index.js
+++ src/features/WorkflowManager/components/ChangeLog/index.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
-// import axios from "axios";
+import axios from "axios";
 import PropTypes from "prop-types";
-// import InfiniteScroll from 'react-infinite-scroller';
-// import LoadingAnimation from "@boomerang/boomerang-components/lib/LoadingAnimation";
+import InfiniteScroll from 'react-infinite-scroller';
+import LoadingAnimation from "@boomerang/boomerang-components/lib/LoadingAnimation";
 import NoDisplay from "@boomerang/boomerang-components/lib/NoDisplay";
 import ScrollUp from "Components/ScrollUp";
 import LogCard from "./LogCard";
-import sortByProp from "@boomerang/boomerang-utilities/lib/sortByProp";
-// import { BASE_SERVICE_URL } from "Config/servicesConfig";
+import { BASE_SERVICE_URL } from "Config/servicesConfig";
 import "./styles.scss";
 
 class ChangeLog extends Component {
@@ -20,51 +19,41 @@ class ChangeLog extends Component {
     workflow: {}
   };
 
-  // state = {
-  //   hasMoreLogs: !this.props.changeLog.data.last,
-  //   changeLogList: this.props.changeLog.data.records
-  // };
+  state = {
+    hasMoreLogs: this.props.changeLog.data.length<10 ? false:true,
+    changeLogList: this.props.changeLog.data
+  };
 
-  // loadMoreLogs = (page) => {
-  //   let newChangeLog = [].concat(this.state.changeLogList);
-  //   axios.get(`${BASE_SERVICE_URL}/workflow/${this.props.workflow.data.id}/changelog?size=10&page=${page}`).then(response => {
-  //     const { records, last } = response.data;
-  //     this.setState({changeLogList: newChangeLog.concat(records), hasMoreLogs:!last});
-  //   });
-  // }
+  loadMoreLogs = (page) => {
+    let newChangeLog = [].concat(this.state.changeLogList);
+    axios.get(`${BASE_SERVICE_URL}/workflow/${this.props.workflow.data.id}/changelog?size=10&page=${page}&sort=version&order=DESC`).then(response => {
+      this.setState({changeLogList: newChangeLog.concat(response.data), hasMoreLogs:response.data.length<10 ? false:true});
+    });
+  }
 
   render() {
-    // const { hasMoreLogs, changeLogList } = this.state; 
-    const { changeLog } = this.props; 
-    const changeLogSorted = sortByProp(changeLog.data, "version", "desc" );
-
+    const { hasMoreLogs, changeLogList } = this.state; 
     return(
       <div className="c-worklfow-change-log">
         <label className="b-worklfow-change-log__title">{`${this.props.workflow.data.name}`}</label>
         <div className="c-worklfow__log-list">
           {
-            changeLogSorted.length>0?
-            // <InfiniteScroll
-            //   pageStart={0}
-            //   loadMore={this.loadMoreLogs}
-            //   hasMore={hasMoreLogs}
-            //   loader={<LoadingAnimation className="s-change-log-loading"/>}
-            //   useWindow={true}
-            // >              
-            //   {
-            //     changeLogList.map(log => {
-            //       return (
-            //         <LogCard log={log} />
-            //       );
-            //     })
-            //   }      
-            // </InfiniteScroll>
-            
-            changeLogSorted.map(log => {
-              return (
-                <LogCard log={log} />
-              );
-            })
+            changeLogList.length>0?
+            <InfiniteScroll
+              pageStart={0}
+              loadMore={this.loadMoreLogs}
+              hasMore={hasMoreLogs}
+              loader={<LoadingAnimation className="s-change-log-loading"/>}
+              useWindow={true}
+            >              
+              {
+                changeLogList.map(log => {
+                  return (
+                    <LogCard log={log} />
+                  );
+                })
+              }      
+            </InfiniteScroll>
             :
             <NoDisplay text="No changes to display" style={{marginTop:"2rem"}}/>
           }
