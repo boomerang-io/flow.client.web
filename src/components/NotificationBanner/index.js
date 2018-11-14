e2e0d7384b98886052f77f2d1252ad6c40ccc2b7
TIMOTHY R. BULA <trbula@us.ibm.com>
2018-11-14 15:54:15 -0500

    feat: add deep linking

--- src/components/NotificationBanner/index.js
+++ src/components/NotificationBanner/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import classnames from "classnames";
+import Link from "@boomerang/boomerang-components/lib/Link";
 import close from "Assets/svg/close.svg";
 import "./styles.scss";
+
+const SLACK_TEAM_ID = "T27TLPNS1";
 
 class NotificationBanner extends Component {
   static propTypes = {
@@ -19,12 +22,30 @@ class NotificationBanner extends Component {
   };
 
   render() {
-    const text =
-      "Welcome to Boomerang Flow. We are excited to have an Alpha release and hope you enjoy using it. Occasionally, you may notice a bug or an issue. Please let us know what goes wrong on Slack: @twlawrie, @trbula or @mdroy";
-
     return (
       <div className={classnames("b-notification-banner", { "--hidden": !this.state.isOpen })}>
-        <div className="b-notification-banner__text">{text}</div>
+        <div className="b-notification-banner__text">
+          Welcome to Boomerang Flow! We are excited to have an Alpha release and hope you enjoy using it. You may notice
+          a bug or two. Please don't hesitate to reach out to us on Slack at{" "}
+          {
+            <Link openInNewContext href={`slack://user?team=${SLACK_TEAM_ID}&id=W3FECR56F`}>
+              @twlawrie
+            </Link>
+          }
+          ,{" "}
+          {
+            <Link openInNewContext href={`slack://user?team=${SLACK_TEAM_ID}&id=W3FECR45R`}>
+              @trbula
+            </Link>
+          }
+          , or{" "}
+          {
+            <Link openInNewContext href={`slack://user?team=${SLACK_TEAM_ID}&id=W3FJMSSQ4`}>
+              @mdroy
+            </Link>
+          }
+          . Thanks!
+        </div>
         <img onClick={this.closeBanner} src={close} className="b-notification-banner__close-icon" alt="close" />
       </div>
     );
