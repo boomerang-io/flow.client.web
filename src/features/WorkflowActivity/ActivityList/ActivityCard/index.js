417ec5eb4818c9a21814671c7e5213308b70b853
Isabela Mayumi Kawabata Borges <isabela.kawabata@ibm.com>
2019-01-08 15:53:54 -0200

    feat(activity): update Upated cards and filters

--- src/features/WorkflowActivity/ActivityList/ActivityCard/index.js
+++ src/features/WorkflowActivity/ActivityList/ActivityCard/index.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import moment from "moment";
 import getHumanizedDuration from "@boomerang/boomerang-utilities/lib/getHumanizedDuration";
 import { ACTIVITY_STATUSES_TO_TEXT, ACTIVITY_STATUSES_TO_ICON } from "Constants/activityStatuses";
-import triggerTypes from "Constants/triggerTypes";
+// import triggerTypes from "Constants/triggerTypes";
 import imgs from "Assets/icons";
 import "./styles.scss";
 
@@ -38,42 +38,51 @@ class ActivityCard extends Component {
         }}
       >
         <div className="c-activity-card__workflow">
-          <div className="c-activity-card__icon">
-            <img className="b-activity-card__icon" src={imgs[icon ? icon : "docs"]} alt="icon" />
+          <div className="c-activity-card__workflow--left">
+            <div className="c-activity-card__icon">
+              <img className="b-activity-card__icon" src={imgs[icon ? icon : "docs"]} alt="icon" />
+            </div>
+            <div className="c-activity-card__workflow-info">
+              <label className="b-activity-card__name">{workflowName}</label>
+              <label className="b-activity-card__description">{description}</label>
+            </div>
           </div>
-          <div className="c-activity-card__workflow-info">
-            <label className="b-activity-card__team-name">{teamName}</label>
-            <label className="b-activity-card__name">{workflowName}</label>
-            <label className="b-activity-card__description">{description}</label>
-          </div>
-          <div className="c-activity-card__workflow-trigger">
-            {trigger && <label className="b-activity-card__trigger-label">Trigger</label>}
-            {trigger === triggerTypes.MANUAL && <label className="b-activity-card__trigger-label">Initiated by</label>}
-          </div>
-          <div className="c-activity-card__workflow-trigger">
-            <label className="b-activity-card__trigger">{trigger}</label>
-            {trigger === triggerTypes.MANUAL && <label className="b-activity-card__autor">{initiatedByUserName}</label>}
-          </div>
-        </div>
-        <div className="c-activity-card__activity">
-          <div className="c-activity-card__label">
-            <label className="b-activity-card__label">Start Time</label>
-            <label className="b-activity-card__data">{moment(creationDate).format("YYYY-MM-DD hh:mm A")}</label>
-          </div>
-          <div className="c-activity-card__label">
-            <label className="b-activity-card__label">Duration</label>
-            <label className="b-activity-card__data">{getHumanizedDuration(parseInt(duration / 1000, 10))}</label>
-          </div>
-          <div className="c-activity-card__label">
-            <label className="b-activity-card__label"> Status</label>
-            <label className="b-activity-card__data">
-              <img
-                className="b-activity-card__status-icon"
-                src={ACTIVITY_STATUSES_TO_ICON[status ? status : "notstarted"]}
-                alt={`Status ${status}`}
-              />
-              {ACTIVITY_STATUSES_TO_TEXT[status]}
-            </label>
+          <div className="c-activity-card__workflow--right">
+            <div className="c-activity-card__workflow-first-column">
+              <div className="c-activity-card__trigger-info">
+                <div className="c-activity-card__workflow-trigger-label">
+                  <label className="b-activity-card__trigger-label">Team</label>
+                  <label className="b-activity-card__trigger-label">Trigger</label>
+                  <label className="b-activity-card__trigger-label">Initiated by</label>
+                </div>
+                <div className="c-activity-card__workflow-trigger-info">
+                  <label className="b-activity-card__team-name">{teamName}</label>
+                  <label className="b-activity-card__trigger">{trigger || "—"}</label>
+                  <label className="b-activity-card__autor">{initiatedByUserName || "—"}</label>
+                </div>
+              </div>
+            </div>
+            <div className="c-activity-card__workflow-second-column">
+              <div className="c-activity-card__label">
+                <label className="b-activity-card__label">Start Time</label>
+                <label className="b-activity-card__data">{moment(creationDate).format("YYYY-MM-DD hh:mm A")}</label>
+              </div>
+              <div className="c-activity-card__label">
+                <label className="b-activity-card__label">Duration</label>
+                <label className="b-activity-card__data">{getHumanizedDuration(parseInt(duration / 1000, 10))}</label>
+              </div>
+              <div className="c-activity-card__label">
+                <label className="b-activity-card__label"> Status</label>
+                <label className="b-activity-card__data">
+                  <img
+                    className="b-activity-card__status-icon"
+                    src={ACTIVITY_STATUSES_TO_ICON[status ? status : "notstarted"]}
+                    alt={`Status ${status}`}
+                  />
+                  {ACTIVITY_STATUSES_TO_TEXT[status]}
+                </label>
+              </div>
+            </div>
           </div>
         </div>
       </Link>
