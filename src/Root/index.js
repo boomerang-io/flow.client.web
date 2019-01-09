c3172d90bdf59e3285f28e6379f253ff665c7877
TIMOTHY R. BULA <trbula@us.ibm.com>
2019-01-09 09:06:23 -0500

    feat: js hot reloading :)

--- src/Root/index.js
+++ src/Root/index.js
@@ -1,27 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
+import { hot } from "react-hot-loader/root";
 import { ConnectedRouter } from "connected-react-router";
 import { Provider } from "react-redux";
 import ErrorBoundary from "@boomerang/boomerang-components/lib/ErrorBoundary";
 import App from "Features/App";
 import ErrorDragon from "Components/ErrorDragon";
 
-export default class Root extends Component {
-  render() {
-    const { store, history } = this.props;
-    return (
-      <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <ErrorBoundary errorComponent={ErrorDragon}>
-            <App />
-          </ErrorBoundary>
-        </ConnectedRouter>
-      </Provider>
-    );
-  }
-}
+const Root = props => {
+  const { store, history } = props;
+  return (
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <ErrorBoundary errorComponent={ErrorDragon}>
+          <App />
+        </ErrorBoundary>
+      </ConnectedRouter>
+    </Provider>
+  );
+};
 
 Root.propTypes = {
   store: PropTypes.object.isRequired,
   history: PropTypes.object.isRequired
 };
+
+export default hot(Root);
