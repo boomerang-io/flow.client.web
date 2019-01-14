d7c8c5e1b402e9e81655fe447ee0694d82372ea5
Benjamin Ruby <Benjamin.Ruby@ibm.com@Benjamins-MacBook-Pro.local>
2019-01-14 10:12:01 -0600

    fix: need styling changes for modal

--- src/components/SwitchLink/index.js
+++ src/components/SwitchLink/index.js
@@ -2,11 +2,26 @@ import React, { Component } from "react";
 import PropTypes from "prop-types"; //to implement
 import Modal from "react-modal";
 import CloseModalButton from "@boomerang/boomerang-components/lib/CloseModalButton";
+<<<<<<< HEAD
 import ModalFlow from "@boomerang/boomerang-components/lib/ModalFlow";
 import ConfigureSwitchModal from "./ConfigureSwitchModal";
 import TriangleArrowIcon from "./TriangleArrow";
 import pencilIcon from "./pencil.svg";
 import "./styles.scss";
+=======
+import TextInput from "@boomerang/boomerang-components/lib/TextInput";
+import TriangleArrow from "./TriangleArrow";
+import pencilIcon from "./pencil.svg";
+import Modal from "react-modal";
+import classnames from "classnames";
+
+import ModalContentBody from "@boomerang/boomerang-components/lib/ModalContentBody";
+import ModalContentHeader from "@boomerang/boomerang-components/lib/ModalContentHeader";
+import ModalContentFooter from "@boomerang/boomerang-components/lib/ModalContentFooter";
+import ModalConfirmButton from "@boomerang/boomerang-components/lib/ModalConfirmButton";
+//import TextInput from "@boomerang/boomerang-components/lib/TextInput";
+import Toggle from "@boomerang/boomerang-components/lib/Toggle";
+>>>>>>> fix: need styling changes for modal
 
 class SwitchLink extends Component {
   constructor(props) {
@@ -22,7 +37,15 @@ class SwitchLink extends Component {
   }
 
   componentDidMount() {
+<<<<<<< HEAD
     //this.props.diagramEngine.repaintCanvas();
+=======
+    this.props.diagramEngine.repaintCanvas();
+    //For accessibility: https://github.com/reactjs/react-modal#app-element
+    if (document.getElementById(this.props.documentRootTagId)) {
+      Modal.setAppElement(`#${this.props.documentRootTagId}`);
+    }
+>>>>>>> fix: need styling changes for modal
   }
 
   componentWillUnmount() {
@@ -77,8 +100,30 @@ class SwitchLink extends Component {
     return true;
   };
 
-  handleClick = () => {
-    console.log("we have been clicked");
+  openModal = () => {
+    this.setState({ modalIsOpen: true });
+  };
+  closeModal = () => {
+    this.setState({ modalIsOpen: false });
+    //also save back the state
+    //this.props.model.switchCondition = this.state.switchCondition;
+    //this.props.diagramEngine.repaintCanvas();
+  };
+
+  handleSave = () => {
+    this.setState({ modalIsOpen: false });
+    //also save back the state
+    this.props.model.switchCondition = this.state.switchCondition;
+    this.props.diagramEngine.repaintCanvas();
+  };
+
+  updateDefaultState = () => {
+    this.setState(prevState => ({ defaultState: !prevState.defaultState }));
+    console.log(this.state);
+    if (this.state.defaultState === true) {
+      console.log("changing state");
+      this.setState({ switchCondition: "default" });
+    }
   };
 
   render() {
@@ -98,6 +143,10 @@ class SwitchLink extends Component {
             </g>
             <g transform={`translate(${this.halfwayPoint.x - 17}, ${this.halfwayPoint.y + 2})`}>
               <foreignObject>
+<<<<<<< HEAD
+=======
+                {/* <EditSwitchButton onClick={this.handleClick} initialSwitchCondition={this.state.switchCondition} /> */}
+>>>>>>> fix: need styling changes for modal
                 <g>
                   <img
                     src={pencilIcon}
@@ -109,11 +158,17 @@ class SwitchLink extends Component {
                     isOpen={this.state.modalIsOpen}
                     onAfterOpen={this.afterOpenModal}
                     onRequestClose={this.closeModal}
+<<<<<<< HEAD
+=======
+                    style={{ backgroundColor: "#fbfcfc", color: "#272727" }}
+                    fullScree
+>>>>>>> fix: need styling changes for modal
                     contentLabel="Example Modal"
                     contentLabel="Modal"
                     documentRootTagId="app"
                     overlayClassName="bmrg--c-modal-overlay"
                     ariaHideApp={true}
+<<<<<<< HEAD
                     className="bmrg--c-modal"
                   >
                     <ModalFlow
@@ -134,10 +189,57 @@ class SwitchLink extends Component {
                         validateSwitch={this.validateSwitch}
                       />
                     </ModalFlow>
+=======
+                    //className={classnames("bmrg--c-modal", { "--full-screen": false })}
+                    className={"bmrg--c-modal"}
+                    defaultState={this.state.default}
+                  >
+                    <form onSubmit={this.handleSave}>
+                      <ModalContentHeader title="Edit Switch Value" subtitle="" theme="bmrg-white" />
+                      <CloseModalButton onClick={this.closeModal} />
+                      <ModalContentBody
+                        style={{ maxWidth: "25rem", margin: "0 auto", flexDirection: "column", overflow: "visible" }}
+                      >
+                        <div className="b-default">
+                          <div className="b-default__desc">Default?</div>
+                          <Toggle
+                            aria-labelledby="toggle-default"
+                            className="b-default__toggle"
+                            name="default"
+                            checked={this.state.defaultState}
+                            onChange={this.updateDefaultState}
+                            theme="bmrg-white"
+                            red
+                          />
+                          <div className="b-default__explanation">
+                            When this switch is on, this connection will be taken only when no others are matched.
+                          </div>
+                        </div>
+
+                        {!this.state.defaultState && (
+                          <TextInput
+                            alwaysShowTitle
+                            required
+                            value={this.state.switchCondition}
+                            title="Switch Property Value"
+                            placeholder="Enter a value"
+                            name="cron"
+                            theme="bmrg-white"
+                            onChange={this.updateSwitchState}
+                            style={{ paddingBottom: "1rem" }}
+                          />
+                        )}
+                      </ModalContentBody>
+                      <ModalContentFooter>
+                        <ModalConfirmButton text="SAVE" theme="bmrg-white" disabled={false} type="submit" />
+                      </ModalContentFooter>
+                    </form>
+>>>>>>> fix: need styling changes for modal
                   </Modal>
                 </g>
               </foreignObject>
             </g>
+<<<<<<< HEAD
             <g transform={`translate(${this.halfwayPoint.x + 18}, ${this.halfwayPoint.y - 6})`}>
               <text className="small">{this.props.model.switchCondition}</text>
               {/*<foreignObject>
@@ -145,6 +247,12 @@ class SwitchLink extends Component {
                   <text className="small">{this.props.model.switchCondition}</text>
                 </div>
               </foreignObject>*/}
+=======
+            <g transform={`translate(${this.halfwayPoint.x - 10}, ${this.halfwayPoint.y + 8})`}>
+              <text x="55" y="55" class="small">
+                {this.props.model.switchCondition}
+              </text>
+>>>>>>> fix: need styling changes for modal
             </g>
           </>
         )}
