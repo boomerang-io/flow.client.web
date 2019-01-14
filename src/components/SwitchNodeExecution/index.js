699b148dc2b67161d8361c6da4196f20f68f2d4c
Benjamin Ruby <Benjamin.Ruby@ibm.com@Benjamins-MacBook-Pro.local>
2019-01-14 16:14:06 -0600

    fix: added styles, added switch execution node

---
+++ src/components/SwitchNodeExecution/index.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actions as taskActions } from "State/tasks";
+import { actions as workflowRevisionActions } from "State/workflowRevision";
+import { PortWidget } from "@boomerang/boomerang-dag";
+import CloseModalButton from "@boomerang/boomerang-components/lib/CloseModalButton";
+import Modal from "@boomerang/boomerang-components/lib/Modal";
+import ModalFlow from "@boomerang/boomerang-components/lib/ModalFlow";
+import Tooltip from "@boomerang/boomerang-components/lib/Tooltip";
+import pencilIcon from "./pencil.svg";
+import { TASK_KEYS_TO_ICON } from "Constants/taskIcons";
+import "./styles.scss";
+
+export class SwitchNode extends Component {
+  static propTypes = {
+    nodeConfig: PropTypes.object.isRequired,
+    task: PropTypes.object.isRequired,
+    taskActions: PropTypes.object.isRequired,
+    workflowRevisionActions: PropTypes.object.isRequired
+  };
+
+  static defaultProps = {
+    nodeConfig: {}
+  };
+
+  state = {};
+
+  handleOnSave = inputs => {
+    this.props.workflowRevisionActions.updateNode({ nodeId: this.props.node.id, inputs });
+    this.forceUpdate();
+  };
+
+  // Delete the node in state and then remove it from the diagram
+  handleOnDelete = () => {
+    this.props.workflowRevisionActions.deleteNode({ nodeId: this.props.node.id });
+    this.props.node.remove();
+  };
+
+  render() {
+    console.log(this.props);
+    return (
+      <div className="b-switchNode">
+        <Tooltip className="custom-node-toolTip" place="left" id={this.props.node.id}>
+          {this.props.task ? this.props.task.description : "Task description"}
+        </Tooltip>
+        <div className="b-switchNode__tile" data-tip data-for={this.props.node.id}>
+          {this.props.nodeConfig.inputs && this.props.nodeConfig.inputs.value
+            ? this.props.nodeConfig.inputs.value
+            : this.props.task.name}
+        </div>
+
+        <PortWidget className="b-switchNode-port --left" name="left" node={this.props.node} />
+        <PortWidget className="b-switchNode-port --right" name="right" node={this.props.node} />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    task: state.tasks.data.find(task => task.id === ownProps.node.taskId),
+    nodeConfig: state.workflowRevision.config[ownProps.node.id]
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  taskActions: bindActionCreators(taskActions, dispatch),
+  workflowRevisionActions: bindActionCreators(workflowRevisionActions, dispatch)
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SwitchNode);
