fe6e20716768ee4fe960d6b43efb81c98dcabf30
Timothy R. Bula <trbula@us.ibm.com>
2019-01-14 18:32:55 -0600

    feat: product service

--- src/config/servicesConfig.js
+++ src/config/servicesConfig.js
@@ -3,8 +3,13 @@ export const BASE_SERVICE_ENV_URL =
     ? "http://localhost:8000"
     : window._SERVER_DATA && window._SERVER_DATA.BASE_SERVICE_ENV_URL;
 
+export const PRODUCT_SERVICE_ENV_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8000"
+    : window._SERVER_DATA && window._SERVER_DATA.PRODUCT_SERVICE_ENV_URL;
+
 // Standard
-export const BASE_SERVICE_URL = `${BASE_SERVICE_ENV_URL}/flow`;
+export const BASE_SERVICE_URL = `${PRODUCT_SERVICE_ENV_URL}/flow`;
 export const BASE_LAUNCHPAD_SERVICE_URL = `${BASE_SERVICE_ENV_URL}/launchpad`;
 
 // Profile image
