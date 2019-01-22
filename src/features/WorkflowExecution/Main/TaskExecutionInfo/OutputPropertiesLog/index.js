5a729a9ca118d07eb148fde174a3613349ef7df7
Isabela Mayumi Kawabata Borges <isabela.kawabata@ibm.com>
2019-01-22 14:15:08 -0200

    feat(execution): output Update output modal to show table props

--- src/features/WorkflowExecution/Main/TaskExecutionInfo/OutputPropertiesLog/index.js
+++ src/features/WorkflowExecution/Main/TaskExecutionInfo/OutputPropertiesLog/index.js
@@ -4,6 +4,9 @@ import ReactJson from "react-json-view";
 import Modal from "@boomerang/boomerang-components/lib/Modal";
 import ModalFlow from "@boomerang/boomerang-components/lib/ModalFlow";
 import ModalContentBody from "@boomerang/boomerang-components/lib/ModalContentBody";
+import Tab from "@boomerang/boomerang-components/lib/Tab";
+import Tabs from "@boomerang/boomerang-components/lib/Tabs";
+import PropertiesTable from "./PropertiesTable";
 import "./styles.scss";
 
 class OutputPropertiesLog extends React.Component {
@@ -19,27 +22,42 @@ class OutputPropertiesLog extends React.Component {
 
   render() {
     const { flowTaskName, flowTaskOutputs } = this.props;
+    let arrayProps = [];
+    Object.keys(flowTaskOutputs).forEach(
+      val => (arrayProps = arrayProps.concat({ key: val, value: JSON.stringify(flowTaskOutputs[val]) }))
+    );
     return (
       <Modal
         ModalTrigger={() => <div className="s-task-log-trigger">Output Properties</div>}
         modalContent={(closeModal, rest) => (
           <ModalFlow
+            style={{ display: "flex", flexDirection: "column", height: "auto" }}
             headerTitle="Output Properties"
             headerSubtitle={flowTaskName}
             closeModal={closeModal}
             theme="bmrg-white"
             {...rest}
           >
-            <ModalContentBody style={{ width: "20rem", display: "block" }}>
-              <ReactJson
-                name={false}
-                src={flowTaskOutputs}
-                displayDataTypes={false}
-                enableDelete={false}
-                displayObjectSize={false}
-                enableEdit={false}
-                enableAdd={false}
-              />
+            <ModalContentBody style={{ width: "40rem", display: "block", alignSelf: "center", height: "30rem" }}>
+              <Tabs
+                theme="bmrg-white"
+                tabsListProps={{ style: { width: "100%", justifyContent: "space-around", marginBottom: "1rem" } }}
+              >
+                <Tab value="Table" name="table">
+                  <PropertiesTable data={arrayProps} />
+                </Tab>
+                <Tab value="JSON" name="json">
+                  <ReactJson
+                    name={false}
+                    src={flowTaskOutputs}
+                    displayDataTypes={false}
+                    enableDelete={false}
+                    displayObjectSize={false}
+                    enableEdit={false}
+                    enableAdd={false}
+                  />
+                </Tab>
+              </Tabs>
             </ModalContentBody>
           </ModalFlow>
         )}
