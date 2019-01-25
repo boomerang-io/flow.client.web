691d38f337679cf6498da48ae7edb6e5dd2732c1
Benjamin Ruby <Benjamin.Ruby@ibm.com@Benjamins-MacBook-Pro.local>
2019-01-24 20:08:38 -0600

    feat: wip new designer task search bar

--- src/features/WorkflowManager/components/TasksSidenav/index.js
+++ src/features/WorkflowManager/components/TasksSidenav/index.js
@@ -1,19 +1,59 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import SearchBar from "@boomerang/boomerang-components/lib/SearchBar";
 import { REQUEST_STATUSES } from "Config/servicesConfig";
 import Tasks from "./Tasks";
+import matchSorter from "match-sorter";
 
 class TasksSidenavContainer extends Component {
   static propTypes = {
     tasks: PropTypes.object.isRequired
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasksToDisplay: props.tasks,
+      searchQuery: ""
+    };
+  }
+
+  handleClear = () => {
+    this.setState({ tasksToDisplay: this.props.tasks, searchQuery: "" }, () => {
+      this.forceUpdate();
+    });
+  };
+
+  handleOnSearchInputChange = e => {
+    const searchQuery = e.target.value;
+    this.setState({ searchQuery }, () => {
+      this.handleSearchFilter();
+    });
+  };
+
+  handleSearchFilter = () => {
+    const { searchQuery, tasksToDisplay } = this.state;
+    let newTasksToDisplay = matchSorter(this.props.tasks.data, searchQuery, { keys: ["category", "name"] });
+    this.setState({ tasksToDisplay: newTasksToDisplay });
+  };
+
   render() {
     const { tasks } = this.props;
 
     if (tasks.status === REQUEST_STATUSES.SUCCESS) {
-      return <Tasks tasks={tasks.data} />;
+      return (
+        <div>
+          <SearchBar
+            theme="bmrg-white"
+            onChange={this.handleOnSearchInputChange}
+            onClear={this.handleClear}
+            value={this.state.searchQuery}
+            debounceTimeout={false}
+          />
+          <Tasks tasks={this.state.tasksToDisplay} />
+        </div>
+      );
     }
 
     return null;
