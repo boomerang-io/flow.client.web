e11b79dd5b8b69d46c8ccad926894205b152525a
Timothy R. Bula <trbula@us.ibm.com>
2019-01-25 15:06:40 -0500

    feat: sidenav search

--- src/features/WorkflowManager/components/TasksSidenav/index.js
+++ src/features/WorkflowManager/components/TasksSidenav/index.js
@@ -1,39 +1,13 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import SearchBar from "@boomerang/boomerang-components/lib/SearchBar";
-import { REQUEST_STATUSES } from "Config/servicesConfig";
 import Tasks from "./Tasks";
-import matchSorter from "match-sorter";
+import { REQUEST_STATUSES } from "Config/servicesConfig";
+import "./styles.scss";
 
 class TasksSidenavContainer extends Component {
   static propTypes = {
     tasks: PropTypes.object.isRequired
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      tasksToDisplay: props.tasks.data,
-      searchQuery: ""
-    };
-  }
-
-  handleClear = () => {
-    this.setState({ tasksToDisplay: this.props.tasks.data, searchQuery: "" });
-  };
-
-  handleOnSearchInputChange = e => {
-    const searchQuery = e.target.value;
-    this.setState({ searchQuery }, () => {
-      this.handleSearchFilter();
-    });
-  };
-
-  handleSearchFilter = () => {
-    const { searchQuery, tasksToDisplay } = this.state;
-    let newTasksToDisplay = matchSorter(this.props.tasks.data, searchQuery, { keys: ["category", "name"] });
-    this.setState({ tasksToDisplay: newTasksToDisplay });
   };
 
   render() {
@@ -41,17 +15,9 @@ class TasksSidenavContainer extends Component {
 
     if (tasks.status === REQUEST_STATUSES.SUCCESS) {
       return (
-        <aside className="b-taskSideNav" style={{ width: "20rem", position: "absolute" }}>
-          <div className="b-taskSearchBar" style={{ width: "20rem", overflow: "hidden" }}>
-            <SearchBar
-              theme="bmrg-white"
-              onChange={this.handleOnSearchInputChange}
-              onClear={this.handleClear}
-              value={this.state.searchQuery}
-            />
-          </div>
-          <Tasks tasks={this.state.tasksToDisplay} />
-        </aside>
+        <div className="c-tasks-sidenav">
+          <Tasks tasks={tasks} />
+        </div>
       );
     }
 
