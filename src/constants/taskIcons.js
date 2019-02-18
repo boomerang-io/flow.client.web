2fd65b5ffcf0c151fd85cbaab4b3779fd45df33d
Benjamin Ruby <Benjamin.Ruby@ibm.com@Benjamins-MacBook-Pro.local>
2019-02-18 16:00:17 -0600

    feat: WIP images

--- src/constants/taskIcons.js
+++ src/constants/taskIcons.js
@@ -3,6 +3,11 @@ import emailIcon from "Assets/svg/email_icon.svg";
 import fileIcon from "Assets/svg/file_icon.svg";
 import settingsIcon from "Assets/svg/settings_icon.svg";
 
+import getIcon from "Assets/svg/get_icon.svg";
+import updateIcon from "Assets/svg/update_icon.svg";
+import deployIcon from "Assets/svg/deploy_icon.svg";
+import validateIcon from "Assets/svg/validate_icon.svg";
+
 /*export const TASK_KEYS = {
   INGEST: "ingest",
   SLACK: "slack",
@@ -36,3 +41,25 @@ export const TASK_KEYS_TO_ICON = {
   [TASK_KEYS.FILE_UTILITIES]: fileIcon,
   [TASK_KEYS.UTILITIES]: settingsIcon
 };
+
+//Get,  Update, deploy
+
+export function iconMapping(taskName, categoryName) {
+  let taskLower = taskName.toLowerCase();
+  let categoryLower = categoryName.toLowerCase();
+
+  if (taskLower.includes("get")) {
+    return getIcon;
+  } else if (taskLower.includes("update")) {
+    return updateIcon;
+  } else if (taskLower.includes("deploy")) {
+    return deployIcon;
+  } else if (taskLower.includes("validate")) {
+    return validateIcon;
+  }
+
+  //default case use categories
+  else {
+    return TASK_KEYS_TO_ICON[categoryLower];
+  }
+}
