fdbf8b75a470ed41433a24059ade5dff841ea9c3
Benjamin Ruby <Benjamin.Ruby@ibm.com@Benjamins-MacBook-Pro.local>
2019-02-19 09:50:48 -0600

    feat: add additional images

--- src/constants/taskIcons.js
+++ src/constants/taskIcons.js
@@ -1,32 +1,18 @@
+//task category images
 import boomerangIcon from "Assets/svg/boomerang_icon.svg";
 import emailIcon from "Assets/svg/email_icon.svg";
 import fileIcon from "Assets/svg/file_icon.svg";
 import settingsIcon from "Assets/svg/settings_icon.svg";
-
+//task name images
 import getIcon from "Assets/svg/get_icon.svg";
 import updateIcon from "Assets/svg/update_icon.svg";
 import deployIcon from "Assets/svg/deploy_icon.svg";
-import validateIcon from "Assets/svg/validate_icon.svg";
-
-/*export const TASK_KEYS = {
-  INGEST: "ingest",
-  SLACK: "slack",
-  SLEEP: "sleep",
-  MAIL: "mail",
-  ARTIFACTORYDOWNLOAD: "artifactoryDownload",
-  CREATEFILE: "createFile",
-  ARTIFACTORYUPLOAD: "artifactoryUpload"
-};
-
-export const TASK_KEYS_TO_ICON = {
-  [TASK_KEYS.INGEST]: downloadIMG,
-  [TASK_KEYS.SLACK]: emailIMG,
-  [TASK_KEYS.SLEEP]: emailIMG,
-  [TASK_KEYS.MAIL]: emailIMG,
-  [TASK_KEYS.ARTIFACTORYDOWNLOAD]: emailIMG,
-  [TASK_KEYS.CREATEFILE]: emailIMG,
-  [TASK_KEYS.ARTIFACTORYUPLOAD]: emailIMG
-};*/
+import statusIcon from "Assets/svg/status_icon.svg";
+import shellIcon from "Assets/svg/shell_icon.svg";
+import enableIcon from "Assets/svg/enable_icon.svg";
+import exportIcon from "Assets/svg/export_icon.svg";
+import downloadIcon from "Assets/svg/download_icon.svg";
+import editIcon from "Assets/svg/edit_icon.svg";
 
 export const TASK_KEYS = {
   BOOMERANG: "boomerang",
@@ -42,23 +28,31 @@ export const TASK_KEYS_TO_ICON = {
   [TASK_KEYS.UTILITIES]: settingsIcon
 };
 
-//Get,  Update, deploy
-
 export function iconMapping(taskName, categoryName) {
   let taskLower = taskName.toLowerCase();
   let categoryLower = categoryName.toLowerCase();
 
-  if (taskLower.includes("get") || taskLower.includes("read")) {
+  if (taskLower.startsWith("get") || taskLower.startsWith("read")) {
     return getIcon;
-  } else if (taskLower.includes("update") || taskLower.includes("sync")) {
+  } else if (taskLower.startsWith("update") || taskLower.startsWith("sync")) {
     return updateIcon;
-  } else if (taskLower.includes("deploy") || taskLower.includes("launch")) {
+  } else if (taskLower.includes("deploy") || taskLower.startsWith("launch")) {
     return deployIcon;
-  } else if (taskLower.includes("validate") || taskLower.includes("verify") || taskLower.includes("check")) {
-    return validateIcon;
+  } else if (taskLower.startsWith("submit") || taskLower.startsWith("validate") || taskLower.includes("approval")) {
+    return statusIcon;
+  } else if (taskLower.startsWith("shell") || taskLower.startsWith("appcmd")) {
+    return shellIcon;
+  } else if (taskLower.startsWith("enable") || taskLower.startsWith("start") || taskLower.startsWith("activate")) {
+    return enableIcon;
+  } else if (taskLower.startsWith("export")) {
+    return exportIcon;
+  } else if (taskLower.includes("download")) {
+    return downloadIcon;
+  } else if (taskLower.includes("edit") || taskLower.includes("define") || taskLower.includes("modify")) {
+    return editIcon;
   }
 
-  //default case use categories
+  //default case use task categories
   else {
     return TASK_KEYS_TO_ICON[categoryLower];
   }
