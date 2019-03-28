5bee4222be1e617af7b2bbe5795ed53a98ba3f4d
TIMOTHY R. BULA <trbula@us.ibm.com>
2019-03-28 19:19:30 -0400

    Update servicesConfig.js

--- src/config/servicesConfig.js
+++ src/config/servicesConfig.js
@@ -11,9 +11,10 @@ export const PRODUCT_SERVICE_ENV_URL =
 // Standard
 export const BASE_SERVICE_URL = `${PRODUCT_SERVICE_ENV_URL}/flow`;
 export const BASE_LAUNCHPAD_SERVICE_URL = `${BASE_SERVICE_ENV_URL}/launchpad`;
+export const BASE_USERS_URL = `${BASE_SERVICE_ENV_URL}/users`;
 
 // Profile image
-export const IMG_URL = `${BASE_LAUNCHPAD_SERVICE_URL}/users/image`;
+export const IMG_URL = `${BASE_USERS_URL}/image`;
 
 export const REQUEST_STATUSES = {
   FAILURE: "failure",
