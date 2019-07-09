f54c07ac0d9e6869b98752c17f67f28d0a9663d6
Benjamin Ruby <Benjamin.Ruby@ibm.com@Benjamins-MacBook-Pro.local>
2019-07-09 13:24:00 -0500

    feat: initial commit w/ new log

--- src/features/WorkflowExecution/Main/TaskExecutionInfo/TaskExecutionLog/index.js
+++ src/features/WorkflowExecution/Main/TaskExecutionInfo/TaskExecutionLog/index.js
@@ -1,14 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
+import cx from "classnames";
 import Modal from "@boomerang/boomerang-components/lib/Modal";
 import ModalFlow from "@boomerang/boomerang-components/lib/ModalFlow";
-// eslint-disable-next-line
-import ReactLazyLog from "react-lazylog"; //TODO: remove need for this. imported to get the styles but not used. es5 doesn't import styles for whatever reason
-import { LazyLog, ScrollFollow } from "react-lazylog/es5";
+import Toggle from "@boomerang/boomerang-components/lib/Toggle";
+import ViewIcon from "@carbon/icons-react/lib/view/16";
+import { LazyLog, ScrollFollow } from "react-lazylog";
 import { BASE_SERVICE_URL } from "Config/servicesConfig";
 import "./styles.scss";
 
-class TaskExecutionLog extends React.Component {
+TaskExecutionLog.propTypes = {
+  flowActivityId: PropTypes.string.isRequired,
+  flowTaskId: PropTypes.string.isRequired,
+  flowTaskName: PropTypes.string.isRequired
+};
+
+function LogViewerTrigger() {
+  return (
+    <div className="b-activity__section">
+      <ViewIcon className="b-activity-actions__icon" alt="View Log" />
+      <div className="b-activity-actions__text">VIEW LOG</div>
+    </div>
+  );
+}
+
+/*class TaskExecutionLog extends React.Component {
   static propTypes = {
     flowActivityId: PropTypes.string.isRequired,
     flowTaskId: PropTypes.string.isRequired,
@@ -50,6 +66,62 @@ class TaskExecutionLog extends React.Component {
       />
     );
   }
-}
+}*/
 
-export default TaskExecutionLog;
+export default function TaskExecutionLog({ flowActivityId, flowTaskId, flowTaskName }) {
+  const [follow, setFollow] = React.useState(true);
+  const [error, setError] = React.useState(false);
+
+  return (
+    <Modal
+      className={cx("bmrg--c-modal", "c-modal-task-log")}
+      modalProps={{ shouldCloseOnOverlayClick: false }}
+      modalTriggerProps={{ tabIndex: "0" }}
+      ModalTrigger={LogViewerTrigger}
+      modalContent={(closeModal, rest) => (
+        <ModalFlow
+          headerTitle="Execution Log"
+          headerSubtitle={flowTaskName}
+          closeModal={closeModal}
+          theme="bmrg-white"
+          {...rest}
+        >
+          <ScrollFollow
+            startFollowing={true}
+            render={({ onScroll }) => (
+              <>
+                <div className="b-task-log-toggle">
+                  <label className={cx("b-task-log-toggle__text", { "--disabled": !!error })} htmlFor="task-log-toggle">
+                    Follow log
+                  </label>
+                  <Toggle
+                    id="task-log-toggle"
+                    checked={follow}
+                    onChange={() => setFollow(!follow)}
+                    defaultValue={follow}
+                    disabled={!!error}
+                    theme="bmrg-white"
+                  />
+                </div>
+
+                <LazyLog
+                  enableSearch={true}
+                  fetchOptions={{
+                    credentials: "include"
+                  }}
+                  follow={follow}
+                  onScroll={onScroll}
+                  onError={err => setError(err)}
+                  selectableLines={true}
+                  stream={true}
+                  url={`${BASE_SERVICE_URL}/activity/${flowActivityId}/log/${flowTaskId}`}
+                  //fetchOptions={{ credentials: "same-origin" }}
+                />
+              </>
+            )}
+          />
+        </ModalFlow>
+      )}
+    />
+  );
+}
