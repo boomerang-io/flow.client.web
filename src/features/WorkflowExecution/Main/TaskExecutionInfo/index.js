f7436d9e56588354caefe12aa8da7aedc2f64565
Benjamin Ruby <Benjamin.Ruby@ibm.com>
2019-07-15 16:32:25 -0500

    Feat 3704 update log viewer (#111)
    * feat: initial commit w/ new log
    
    * fix: added env check to show data locally
    
    * feat: initial commit w/ new log
    
    * fix: added env check to show data locally
    
    * fix: merge from develop
    
    * chore: update lock file
    
    * chore: cleanup unneeded styles
    
    * style: update the style of many a thing

--- src/features/WorkflowExecution/Main/TaskExecutionInfo/index.js
+++ src/features/WorkflowExecution/Main/TaskExecutionInfo/index.js
@@ -9,53 +9,51 @@ import "./styles.scss";
 
 const TaskExecutionInfo = ({ flowActivityId, task }) => {
   return (
-    <div className="c-step-side-info">
-      <div className="s-step-side-info-title">{task.taskName}</div>
-      <div className="c-step-side-info-details">
-        <div className="b-step-side-info-field">
-          <div className="b-step-side-info-field__key">Type</div>
-          <div className="b-step-side-info-field__value">{"Task"}</div>
-        </div>
-        <div className="b-step-side-info-field">
-          <div className="b-step-side-info-field__key">Status</div>
-          <div className="b-step-side-info-field__value">
+    <aside className="c-step-side-info">
+      <h1 className="s-step-side-info-title">{task.taskName}</h1>
+      <section className="c-step-side-info-details">
+        <dl className="b-step-side-info-field">
+          <dt className="b-step-side-info-field__key">Type</dt>
+          <dd className="b-step-side-info-field__value">{"Task"}</dd>
+        </dl>
+        <dl className="b-step-side-info-field">
+          <dt className="b-step-side-info-field__key">Status</dt>
+          <dd className="b-step-side-info-field__value">
             {ACTIVITY_STATUSES_TO_TEXT[task.flowTaskStatus]}
             <img src={ACTIVITY_STATUSES_TO_ICON[task.flowTaskStatus]} alt="Status" />
-          </div>
-        </div>
-        <div className="b-step-side-info-field">
-          <div className="b-step-side-info-field__key">Start Time</div>
-          <div className="b-step-side-info-field__value">
-            {moment(task.startTime).format("MMMM Do YYYY, h:mm:ss a")}
-          </div>
-        </div>
-        <div className="b-step-side-info-field">
-          <div className="b-step-side-info-field__key">Duration</div>
-          <div className="b-step-side-info-field__value">
+          </dd>
+        </dl>
+        <dl className="b-step-side-info-field">
+          <dt className="b-step-side-info-field__key">Start Time</dt>
+          <dd className="b-step-side-info-field__value">{moment(task.startTime).format("MMMM Do YYYY, h:mm:ss a")}</dd>
+        </dl>
+        <dl className="b-step-side-info-field">
+          <dt className="b-step-side-info-field__key">Duration</dt>
+          <dd className="b-step-side-info-field__value">
             {task.duration ? getHumanizedDuration(Math.round(parseInt(task.duration / 1000), 10)) : "---"}
-          </div>
-        </div>
-        <div className="b-step-side-info-field">
-          <div className="b-step-side-info-field__key">Log</div>
-          <div className="b-step-side-info-field__value">
+          </dd>
+        </dl>
+        <dl className="b-step-side-info-field">
+          <dt className="b-step-side-info-field__key">Log</dt>
+          <dd className="b-step-side-info-field__value">
             <TaskExecutionLog
               flowTaskStatus={task.flowTaskStatus}
               flowTaskId={task.taskId}
               flowActivityId={flowActivityId}
               flowTaskName={task.taskName}
             />
-          </div>
-        </div>
+          </dd>
+        </dl>
         {task.outputs && Object.keys(task.outputs).length > 0 && (
-          <div className="b-step-side-info-field">
-            <div className="b-step-side-info-field__key">Properties</div>
-            <div className="b-step-side-info-field__value">
+          <dl className="b-step-side-info-field">
+            <dt className="b-step-side-info-field__key">Properties</dt>
+            <dd className="b-step-side-info-field__value">
               <OutputPropertiesLog flowTaskName={task.taskName} flowTaskOutputs={task.outputs} />
-            </div>
-          </div>
+            </dd>
+          </dl>
         )}
-      </div>
-    </div>
+      </section>
+    </aside>
   );
 };
 
