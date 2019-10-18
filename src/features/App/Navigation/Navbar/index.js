589aa2978e3b7830677bc50a22c32a6ca1ea12b4
Timothy R. Bula <trbula@us.ibm.com>
2019-10-17 22:11:19 -0400

    feat: update sidenav

--- src/features/App/Navigation/Navbar/index.js
+++ src/features/App/Navigation/Navbar/index.js
@@ -1,83 +1,90 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import {
-  UIShell,
-  InteriorLeftNav,
-  InteriorLeftNavItem,
-  InteriorLeftNavList
-} from "@boomerang/carbon-addons-boomerang-react";
+import { LeftSideNav, UIShell } from "@boomerang/carbon-addons-boomerang-react";
 import SERVICE_REQUEST_STATUSES from "Constants/serviceRequestStatuses";
-import { BASE_APPS_ENV_URL, BASE_LAUNCH_ENV_URL } from "Config/platformUrlConfig";
-import { APP_ROOT } from "Config/appConfig";
+import { BASE_LAUNCH_ENV_URL } from "Config/platformUrlConfig";
 import { BASE_URL } from "Config/servicesConfig";
 import { NavLink } from "react-router-dom";
+import { Activity16, ChartScatter16, FlowData16, SettingsAdjust16 } from "@carbon/icons-react";
+import { SideNav, SideNavLink, SideNavItems, SideNavMenu, SideNavMenuItem } from "carbon-components-react";
 
-const FLOW_PATH = `${BASE_APPS_ENV_URL}${APP_ROOT}`;
-const baseLaunchUrl = new URL(BASE_LAUNCH_ENV_URL);
-const baseURL = baseLaunchUrl.origin;
-
-const onMenuClick = ({ isOpen, onMenuClose }) => (
-  <InteriorLeftNav isOpen={isOpen} onMenuClose={onMenuClose}>
-    <InteriorLeftNavItem baseURL={baseURL} href={`${FLOW_PATH}/workflows`} label="Workflows">
-      <NavLink to="/workflows" exact={false}>
-        Workflows
-      </NavLink>
-    </InteriorLeftNavItem>
-    <InteriorLeftNavItem baseURL={baseURL} href={`${FLOW_PATH}/activity`} label="Activity">
-      <NavLink to="/activity" exact={false}>
-        Activity
-      </NavLink>
-    </InteriorLeftNavItem>
-    {/*  <InteriorLeftNavItem baseURL={baseURL} href={`${FLOW_PATH}/creator/overview`} label="Designer">
-      <NavLink to="/creator/overview" exact={false}>
-        Designer
-      </NavLink>
-    </InteriorLeftNavItem> */}
-    <InteriorLeftNavItem baseURL={baseURL} href={`${FLOW_PATH}/insights`} label="Insights">
-      <NavLink to="/insights" exact={false}>
-        Insights
-      </NavLink>
-    </InteriorLeftNavItem>
-    <InteriorLeftNavList title="Manage">
-      <InteriorLeftNavItem baseURL={baseURL} href={`${FLOW_PATH}/properties`} label="Properties">
-        <NavLink to="/properties" exact={false}>
-          Properties
-        </NavLink>
-      </InteriorLeftNavItem>
-    </InteriorLeftNavList>
-  </InteriorLeftNav>
+const onMenuClick2 = ({ isOpen, onMenuClose }) => (
+  <LeftSideNav isOpen={isOpen}>
+    <SideNav expanded={isOpen} isChildOfHeader={true}>
+      <SideNavItems>
+        <SideNavLink
+          activeClassName={"bx--side-nav__link--current"}
+          element={NavLink}
+          onClick={onMenuClose}
+          renderIcon={FlowData16}
+          to="/workflows"
+          large
+        >
+          Workflows
+        </SideNavLink>
+        <SideNavLink
+          activeClassName={"bx--side-nav__link--current"}
+          element={NavLink}
+          onClick={onMenuClose}
+          renderIcon={Activity16}
+          to="/activity"
+          large
+        >
+          Activity
+        </SideNavLink>
+        <SideNavLink
+          activeClassName={"bx--side-nav__link--current"}
+          element={NavLink}
+          onClick={onMenuClose}
+          renderIcon={ChartScatter16}
+          to="/insights"
+          large
+        >
+          Insights
+        </SideNavLink>
+        <SideNavMenu renderIcon={SettingsAdjust16} title="Manage" large>
+          <SideNavMenuItem
+            activeClassName={"bx--side-nav__link--current"}
+            element={NavLink}
+            onClick={onMenuClose}
+            to="/properties"
+          >
+            Properties
+          </SideNavMenuItem>
+        </SideNavMenu>
+      </SideNavItems>
+    </SideNav>
+  </LeftSideNav>
 );
 
 const defaultUIShellProps = {
   baseServiceUrl: BASE_URL,
   renderLogo: true,
-  onMenuClick: onMenuClick,
+  onMenuClick: onMenuClick2,
   baseLaunchEnvUrl: BASE_LAUNCH_ENV_URL
 };
 
-class NavbarContainer extends Component {
-  static propTypes = {
-    navigation: PropTypes.object.isRequired,
-    handleOnTutorialClick: PropTypes.func.isRequired,
-    user: PropTypes.object
-  };
+NavbarContainer.propTypes = {
+  navigation: PropTypes.object.isRequired,
+  handleOnTutorialClick: PropTypes.func.isRequired,
+  user: PropTypes.object
+};
 
-  render() {
-    const { handleOnTutorialClick, navigation, user } = this.props;
+function NavbarContainer(props) {
+  const { handleOnTutorialClick, navigation, user } = props;
 
-    if (navigation.status === SERVICE_REQUEST_STATUSES.SUCCESS && user.status === SERVICE_REQUEST_STATUSES.SUCCESS) {
-      return (
-        <UIShell
-          {...defaultUIShellProps}
-          headerConfig={navigation.data}
-          onTutorialClick={handleOnTutorialClick}
-          user={user.data}
-        />
-      );
-    }
-
-    return <UIShell {...defaultUIShellProps} />;
+  if (navigation.status === SERVICE_REQUEST_STATUSES.SUCCESS && user.status === SERVICE_REQUEST_STATUSES.SUCCESS) {
+    return (
+      <UIShell
+        {...defaultUIShellProps}
+        headerConfig={navigation.data}
+        onTutorialClick={handleOnTutorialClick}
+        user={user.data}
+      />
+    );
   }
+
+  return <UIShell {...defaultUIShellProps} />;
 }
 
 export default NavbarContainer;
