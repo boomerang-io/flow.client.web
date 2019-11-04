e6b9acbec644f769720b33b582144ec920d20b3b
Ben Ruby <benjamin.ruby@ibm.com>
2019-11-04 16:25:19 -0600

    fix: update webhook token text font in settings screen

--- api/middleware/webhook.js
+++ api/middleware/webhook.js
@@ -4,7 +4,10 @@ module.exports = (req, res, next) => {
   const _send = res.send;
   res.send = function(body) {
     if (require("url").parse(req.url, true).query["webhook"]) {
-      return _send.call(this, JSON.stringify({ token: "testToken" }));
+      return _send.call(
+        this,
+        JSON.stringify({ token: "86129CBEDDF8C58C3216B282BF219D005BF92615C4787F4AF73E695C104FB8B5" })
+      );
     }
     return _send.call(this, body);
   };
