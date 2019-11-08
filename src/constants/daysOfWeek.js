a5cca3db4409c39eb6ab783e0d5875ef787d1605
Ben Ruby <benjamin.ruby@ibm.com>
2019-11-08 16:50:19 -0600

    chore: clean up for linting errors

--- src/constants/daysOfWeek.js
+++ src/constants/daysOfWeek.js
@@ -1,9 +1,9 @@
 export default [
-  { label: "Monday", value: "monday", id: "monday", cron: "MON", cronNumber: ["1"] },
-  { label: "Tuesday", value: "tuesday", id: "tuesday", cron: "TUE", cronNumber: ["2"] },
-  { label: "Wednesday", value: "wednesday", id: "wednesday", cron: "WED", cronNumber: ["3"] },
-  { label: "Thursday", value: "thursday", id: "thursday", cron: "THU", cronNumber: ["4"] },
-  { label: "Friday", value: "friday", id: "friday", cron: "FRI", cronNumber: ["5"] },
-  { label: "Saturday", value: "saturday", id: "saturday", cron: "SAT", cronNumber: ["6"] },
-  { label: "Sunday", value: "sunday", id: "sunday", cron: "SUN", cronNumber: ["0", "7"] }
+  { label: "Monday", value: "monday", id: "monday", cron: "MON", cronNumber: ["1"], key: "monday" },
+  { label: "Tuesday", value: "tuesday", id: "tuesday", cron: "TUE", cronNumber: ["2"], key: "tuesday" },
+  { label: "Wednesday", value: "wednesday", id: "wednesday", cron: "WED", cronNumber: ["3"], key: "wednesday" },
+  { label: "Thursday", value: "thursday", id: "thursday", cron: "THU", cronNumber: ["4"], key: "thursday" },
+  { label: "Friday", value: "friday", id: "friday", cron: "FRI", cronNumber: ["5"], key: "friday" },
+  { label: "Saturday", value: "saturday", id: "saturday", cron: "SAT", cronNumber: ["6"], key: "saturday" },
+  { label: "Sunday", value: "sunday", id: "sunday", cron: "SUN", cronNumber: ["0", "7"], key: "sunday" }
 ];
