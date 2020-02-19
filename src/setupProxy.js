9ac3cf4a884e5073bd82688d08a056143a680a16
Andrei Miron <Andrei.Miron1@ibm.com>
2020-02-19 17:27:20 +0200

    chore: [PLT-388] update port forwarding (#170)
    * chore: [PLT-388] update port forwarding
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,14 +1,20 @@
 const proxy = require("http-proxy-middleware");
 const portForwardMap = require("./setupPortForwarding");
+const REACT_APP_PORT_FORWARD = process.env.REACT_APP_PORT_FORWARD;
 
 module.exports = function(app) {
-  Object.entries(portForwardMap).forEach(([path, port]) => {
-    app.use(
-      path,
-      proxy({
-        target: `http://localhost:${port}`,
-        changeOrigin: true
-      })
-    );
-  });
+  if (REACT_APP_PORT_FORWARD) {
+    Object.entries(portForwardMap).forEach(([path, port]) => {
+      if (!path) {
+        return;
+      }
+      app.use(
+        path,
+        proxy({
+          target: `http://localhost:${port}`,
+          changeOrigin: true
+        })
+      );
+    });
+  }
 };
