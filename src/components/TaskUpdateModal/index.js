fe34e671a08abca6b1816359e83960478e18920b
Timothy R. Bula <trbula@us.ibm.com>
2020-04-30 22:49:16 -0400

    feat: handle edge case w/ versions

--- src/components/TaskUpdateModal/index.js
+++ src/components/TaskUpdateModal/index.js
@@ -6,9 +6,10 @@ import {
   Button,
   DataDrivenInput,
   DynamicFormik,
+  Error404,
   ModalForm,
   ModalBody,
-  ModalFooter,
+  ModalFooter
   //TextInput,
 } from "@boomerang/carbon-addons-boomerang-react";
 import TextEditorModal from "Components/TextEditorModal";
@@ -17,13 +18,13 @@ import { WarningFilled16, WarningAlt16 } from "@carbon/icons-react";
 import styles from "./taskUpdateModal.module.scss";
 
 TaskUpdateModal.propTypes = {
-  closeModal: PropTypes.func.isRequired,
+  closeModal: PropTypes.func.isRequired
 };
 
 const UpdateType = {
   Add: "add",
   Remove: "remove",
-  NoChange: "none",
+  NoChange: "none"
 };
 
 // const AutoSuggestInput = (props) => {
@@ -36,7 +37,7 @@ const UpdateType = {
 //   );
 // };
 
-const TextEditorInput = (props) => {
+const TextEditorInput = props => {
   return (
     <div key={props.id} style={{ position: "relative", cursor: "pointer", paddingBottom: "1rem" }}>
       <TextEditorModal {...props} {...props.item} />
@@ -56,9 +57,9 @@ const TextEditorInput = (props) => {
  * }
  */
 function formatAutoSuggestProperties(inputProperties) {
-  return inputProperties.map((property) => ({
+  return inputProperties.map(property => ({
     value: `\${p:${property.key}}`,
-    label: property.key,
+    label: property.key
   }));
 }
 
@@ -66,19 +67,19 @@ const formikSetFieldValue = (value, id, setFieldValue) => {
   setFieldValue(id, value);
 };
 
-const textAreaProps = (inputProperties) => ({ input, formikProps }) => {
+const textAreaProps = inputProperties => ({ input, formikProps }) => {
   const { values, setFieldValue } = formikProps;
   const { key, type, ...rest } = input;
   const itemConfig = TEXT_AREA_TYPES[type];
 
   return {
     autoSuggestions: formatAutoSuggestProperties(inputProperties),
-    formikSetFieldValue: (value) => formikSetFieldValue(value, key, setFieldValue),
+    formikSetFieldValue: value => formikSetFieldValue(value, key, setFieldValue),
     initialValue: values[key],
     inputProperties: inputProperties,
     item: input,
     ...itemConfig,
-    ...rest,
+    ...rest
   };
 };
 
@@ -102,23 +103,36 @@ const textAreaProps = (inputProperties) => ({ input, formikProps }) => {
 
 const toggleProps = ({ input, formikProps }) => {
   return {
-    orientation: "vertical",
+    orientation: "vertical"
   };
 };
 
 export default function TaskUpdateModal({ closeModal, inputProperties, nodeConfig, onSave, task }) {
-  const currentTaskTemplateVersion = task.revisions.find((revision) => revision.version === nodeConfig.taskVersion);
+  const currentTaskTemplateVersion = task.revisions.find(revision => revision.version === nodeConfig.taskVersion);
   const newTaskTemplateVersion = task.revisions[task.revisions.length - 1];
 
+  // Handle edge case of not finding the versions for some reason
+  if (!currentTaskTemplateVersion?.config || !newTaskTemplateVersion?.config) {
+    return (
+      <ModalForm>
+        <Error404
+          header={null}
+          title="Something's off here"
+          message="We couldn't find ones of the tasks needed for the upgrade. Please contact support for assistance upgrading."
+        />
+      </ModalForm>
+    );
+  }
+
   const removedInputs = currentTaskTemplateVersion.config
-    .filter((input) => !newTaskTemplateVersion.config.find((newInput) => newInput.key === input.key))
-    .map((input) => input?.key);
+    .filter(input => !newTaskTemplateVersion.config.find(newInput => newInput.key === input.key))
+    .map(input => input?.key);
 
   const addedInputs = newTaskTemplateVersion.config
-    .filter((input) => !currentTaskTemplateVersion.config.find((currentInput) => currentInput.key === input.key))
-    .map((input) => input?.key);
+    .filter(input => !currentTaskTemplateVersion.config.find(currentInput => currentInput.key === input.key))
+    .map(input => input?.key);
 
-  const handleSubmit = (values) => {
+  const handleSubmit = values => {
     onSave({ version: newTaskTemplateVersion.version, inputs: values });
     closeModal();
   };
@@ -130,7 +144,7 @@ export default function TaskUpdateModal({ closeModal, inputProperties, nodeConfi
       onSubmit={handleSubmit}
       dataDrivenInputProps={{
         //TextInput: AutoSuggestInput,
-        TextEditor: TextEditorInput,
+        TextEditor: TextEditorInput
       }}
       //textAreaProps={textAreaProps(inputProperties)}
       textEditorProps={textAreaProps(inputProperties)}
@@ -147,7 +161,7 @@ export default function TaskUpdateModal({ closeModal, inputProperties, nodeConfi
                 subtitle="Current version in this workflow"
                 version={currentTaskTemplateVersion.version}
               >
-                {currentTaskTemplateVersion.config.map((input) => (
+                {currentTaskTemplateVersion.config.map(input => (
                   <StateHilighter
                     key={input.key}
                     type={removedInputs.includes(input.key) ? UpdateType.Remove : UpdateType.NoChange}
@@ -169,7 +183,7 @@ export default function TaskUpdateModal({ closeModal, inputProperties, nodeConfi
                 subtitle="Latest version available"
                 version={newTaskTemplateVersion.version}
               >
-                {inputs.map((input) => (
+                {inputs.map(input => (
                   <StateHilighter
                     key={input.props.id}
                     type={addedInputs.includes(input.props.id) ? UpdateType.Add : UpdateType.NoChange}
@@ -216,13 +230,13 @@ const ChangeToAppearanceMap = {
   [UpdateType.Add]: {
     icon: <WarningAlt16 fill="#DA1E28" />,
     className: "add",
-    text: "This field has been added.",
+    text: "This field has been added."
   },
   [UpdateType.Remove]: {
     icon: <WarningFilled16 fill="#F1C21B" />,
     className: "remove",
-    text: "This field has been removed.",
-  },
+    text: "This field has been removed."
+  }
 };
 function StateHilighter({ children, hidden, type }) {
   const { className, icon, text } = ChangeToAppearanceMap[type] ?? {};
