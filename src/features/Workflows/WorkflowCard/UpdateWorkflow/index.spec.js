74127a6fd3fb4fb7d332ed686f2dba14c1212362
Timothy R. Bula <trbula@us.ibm.com>
2020-05-20 19:57:44 -0400

    test: update snapshots

--- src/features/Workflows/WorkflowCard/UpdateWorkflow/index.spec.js
+++ src/features/Workflows/WorkflowCard/UpdateWorkflow/index.spec.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { actions as importWorkflowActions, initialState } from "State/importWorkflow";
 import ImportWorkflow from ".";
 import { fireEvent } from "@testing-library/react";
 
@@ -8,7 +7,6 @@ const mockfn = jest.fn();
 const props = {
   fetchTeams: mockfn,
   handleImportWorkflow: mockfn,
-  importWorkflowActions,
   workflowId: "test",
 };
 
@@ -18,9 +16,7 @@ beforeEach(() => {
 
 describe("ImportWorkflow --- Snapshot Test", () => {
   it("Capturing Snapshot of ImportWorkflow", () => {
-    const { baseElement, getByText } = rtlContextRouterRender(<ImportWorkflow {...props} />, {
-      initialState: { importWorkflow: initialState },
-    });
+    const { baseElement, getByText } = rtlContextRouterRender(<ImportWorkflow {...props} />);
     fireEvent.click(getByText(/Choose a file or drag one here/i));
     expect(baseElement).toMatchSnapshot();
   });
