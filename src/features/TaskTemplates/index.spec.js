e9caa21399f2b7a3ff4b3e5d88e710c84ed5b252
Timothy R. Bula <trbula@us.ibm.com>
2020-05-29 19:35:56 -0400

    test: add simple task templates test

---
+++ src/features/TaskTemplates/index.spec.js
@@ -0,0 +1,29 @@
+import React from "react";
+import TaskTemplateManager from "./index";
+import { Route } from "react-router-dom";
+import { startApiServer } from "../../apiServer";
+import { appPath, appLink } from "Config/appConfig";
+import { waitFor } from "@testing-library/react";
+
+let server;
+
+beforeEach(() => {
+  server = startApiServer();
+});
+
+afterEach(() => {
+  server.shutdown();
+});
+
+describe("TaskTemplateManager --- Snapshot", () => {
+  it("Capturing Snapshot of Editor", async () => {
+    const { baseElement, getByText } = rtlContextRouterRender(
+      <Route path={appPath.taskTemplates}>
+        <TaskTemplateManager />
+      </Route>,
+      { route: appLink.taskTemplates() }
+    );
+    await waitFor(() => getByText(/Task manager/i));
+    expect(baseElement).toMatchSnapshot();
+  });
+});
