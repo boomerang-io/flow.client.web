2714bf9dead8bfe3543040982ca92df82ecf956e
Timothy R. Bula <trbula@us.ibm.com>
2020-06-17 19:54:29 -0400

    chore: update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,13 @@
 module.exports = {
-  extends: ["react-app", "plugin:jest/recommended", "plugin:jsx-a11y/recommended", "plugin:cypress/recommended"],
-  plugins: ["jest", "jsx-a11y", "cypress"],
+  extends: [
+    "react-app",
+    "plugin:cypress/recommended",
+    "plugin:jest/recommended",
+    "plugin:jest-dom/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:testing-library/react",
+  ],
+  plugins: ["jest", "jest-dom", "jsx-a11y", "testing-library"],
   env: {
     "jest/globals": true,
     "cypress/globals": true,
@@ -14,15 +21,8 @@ module.exports = {
     rtlReduxRender: true,
     rtlRouterRender: true,
     rtlReduxRouterRender: true,
-    rtlContextRouterRender: true,
   },
   overrides: [
-    {
-      files: ["*.js"],
-      rules: {
-        "jsx-a11y/no-autofocus": 0,
-      },
-    },
     {
       files: ["cypress/**/*.spec.js"],
       rules: {
