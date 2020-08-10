42ea2a8adebaa39143cab994155b7975dd8afdbd
Timothy R. Bula <trbula@us.ibm.com>
2020-08-10 15:24:23 -0400

    feat(settings): update ts impl, add unit and cypress tests, update testing config

--- src/Features/Settings/Settings.tsx
+++ src/Features/Settings/Settings.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { useQuery, useMutation } from "react-query";
 import { Box } from "reflexbox";
-import { Accordion, Loading, notify, ToastNotification, Error404, ErrorMessage } from "@boomerang-io/carbon-addons-boomerang-react";
+import {
+  Accordion,
+  Error404,
+  ErrorMessage,
+  SkeletonPlaceholder,
+  notify,
+  ToastNotification,
+} from "@boomerang-io/carbon-addons-boomerang-react";
 import FeatureHeader from "Components/FeatureHeader";
 import SettingsSection from "./SettingsSection";
 import sortBy from "lodash/sortBy";
-import { serviceUrl, resolver } from "Config/servicesConfig"; 
+import { serviceUrl, resolver } from "Config/servicesConfig";
+import { DataDrivenInput } from "Types";
 import styles from "./settings.module.scss";
+
+export type SettingsGroup = {
+  name: string;
+  key: string;
+  config: DataDrivenInput[];
+};
 
 const platformSettingsUrl = serviceUrl.resourceSettings();
 
@@ -18,12 +32,11 @@ const FeatureLayout: React.FC = ({ children }) => {
         <p>Adjust Flow settings</p>
       </FeatureHeader>
       <Box p="2rem" overflowY="auto" className={styles.container}>
-          {children}
+        {children}
       </Box>
     </>
   );
 };
-  
 
 const Settings: React.FC = () => {
   const { data, error, isLoading } = useQuery({
@@ -33,9 +46,13 @@ const Settings: React.FC = () => {
 
   const [updateSettingMutator] = useMutation(resolver.putPlatformSettings);
 
-  const handleOnSave = async (values: any, config: any, setFieldError: any) => {
-    const newConfig = config.config.map((input: any) => ({ ...input, value: values[input.key] }));
-    const requestBody = [{ ...config, config: newConfig }];
+  const handleOnSave = async (
+    values: { [key: string]: any },
+    settingsGroup: SettingsGroup,
+    setFieldError: (key: string, value: string) => void
+  ) => {
+    const newConfig = settingsGroup.config.map((input: any) => ({ ...input, value: values[input.key] }));
+    const requestBody = [{ ...settingsGroup, config: newConfig }];
     try {
       await updateSettingMutator({ body: requestBody });
       notify(<ToastNotification title="Update Settings" subtitle="Settings succesfully updated" kind="success" />);
@@ -46,11 +63,25 @@ const Settings: React.FC = () => {
   };
 
   if (isLoading) {
-    return <FeatureLayout><Loading /></FeatureLayout>
+    return (
+      <FeatureLayout>
+        <Box className={styles.skeletonPlacholderContainer}>
+          <SkeletonPlaceholder />
+          <SkeletonPlaceholder />
+          <SkeletonPlaceholder />
+          <SkeletonPlaceholder />
+          <SkeletonPlaceholder />
+        </Box>
+      </FeatureLayout>
+    );
   }
 
-  if (error) { 
-    return <FeatureLayout><ErrorMessage /></FeatureLayout>
+  if (error) {
+    return (
+      <FeatureLayout>
+        <ErrorMessage />
+      </FeatureLayout>
+    );
   }
 
   const sortedPlatformSettings = sortBy(data, (settingObj) => settingObj.name);
@@ -59,14 +90,14 @@ const Settings: React.FC = () => {
       {!sortedPlatformSettings.length ? (
         <Error404 header={null} title="No settings found" message={null} />
       ) : (
-        <Accordion style={{ padding: "1.5rem" }}>
-          {sortedPlatformSettings.map((config, index) => (
-            <SettingsSection config={config} index={index} key={index} onSave={handleOnSave} />
+        <Accordion>
+          {sortedPlatformSettings.map((settingsGroup, index) => (
+            <SettingsSection index={index} key={index} onSave={handleOnSave} settingsGroup={settingsGroup} />
           ))}
         </Accordion>
       )}
     </FeatureLayout>
   );
-}
+};
 
 export default Settings;
