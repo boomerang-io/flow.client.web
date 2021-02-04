8a4835fb86adcdd7c728511cdc106eb3f7815043
Ben Ruby <benjamin.ruby@ibm.com>
2021-02-04 10:50:02 -0700

    fix: admin edit system workflow execution

--- src/Features/Execution/Main/WorkflowActions/WorkflowActions.js
+++ src/Features/Execution/Main/WorkflowActions/WorkflowActions.js
@@ -17,8 +17,10 @@ function WorkflowActions({ workflow }) {
   let history = useHistory();
   const { id, scope } = workflow;
   const { user } = useAppContext();
-  const { platformRole } = user;
-  const systemWorkflowsEnabled = allowedUserRoles.includes(platformRole);
+  const { type } = user;
+  console.log(`platformRole: ${type}`);
+  const systemWorkflowsEnabled = allowedUserRoles.includes(type);
+  console.log(`systemWorkflowsEnabled: ${systemWorkflowsEnabled}`);
 
   //don't show the edit workflow button if the workflow has system scope and the user doesn't have permission
   const showEditWorkflow = scope === "team" || (scope === "system" && systemWorkflowsEnabled);
