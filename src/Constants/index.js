84092c1e6ed723b3d8746ecef13f4e4cf4337728
Ben Ruby <benjamin.ruby@ibm.com>
2021-05-05 20:43:18 -0600

    chore: update yaml helper markdown

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -189,5 +189,54 @@ export const ApprovalStatus = {
 
 export const allowedUserRoles = [UserType.Admin, UserType.Operator];
 
-export const yamlInstructions =
-  "# Getting started with a [Tekton Task model](https://tekton.dev/docs/pipelines/tasks/#configuring-a-task)\nTasks in Boomerang Flow follow the Tekton Task model along with Kubernetes standards and allow you to define what you want to happen at the execution of the task as well as parameters that are needed.\nFor more information, see [Getting to know Tasks](https://www.useboomerang.io/docs/boomerang-flow/getting-to-know/tasks).\n## Creating a Task in YAML\nThe YAML specification has three important sections to be aware of: metadata, params, and steps. \nIts important to note that the full Tekton task specification is not yet fully supported. We cannot run multi step tasks, nor do we allow resources to be specified. For more information, see [Known Issues and Limitations](https://www.useboomerang.io/docs/boomerang-flow/introduction/known-issues-limitations)\n### Metadata\nThis is the area where we store a series of annotations related to the end user experience.\n### Params\nThis is where you define Parametes that are needed for the exeuction of the task.\n### Steps\nThis is where xyz.";
+// export const yamlInstructions =
+//   "# Getting started with a [Tekton Task model](https://tekton.dev/docs/pipelines/tasks/#configuring-a-task)\nTasks in Boomerang Flow follow the Tekton Task model along with Kubernetes standards and allow you to define what you want to happen at the execution of the task as well as parameters that are needed.\nFor more information, see [Getting to know Tasks](https://www.useboomerang.io/docs/boomerang-flow/getting-to-know/tasks).\n## Creating a Task in YAML\nThe YAML specification has three important sections to be aware of: metadata, params, and steps. \nIts important to note that the full Tekton task specification is not yet fully supported. We cannot run multi step tasks, nor do we allow resources to be specified. For more information, see [Known Issues and Limitations](https://www.useboomerang.io/docs/boomerang-flow/introduction/known-issues-limitations)\n### Metadata\nThis is the area where we store a series of annotations related to the end user experience.\n### Params\nThis is where you define Parametes that are needed for the exeuction of the task.\n### Steps\nThis is where xyz.";
+
+export const yamlInstructions = `  # Getting started with a Task
+  Tasks in Boomerang Flow follow the [Tekton Task model](https://tekton.dev/docs/pipelines/tasks/#configuring-a-task) along with Kubernetes standards and allow you to define what you want to happen at the execution of the task as well as parameters that are needed.
+  For more information, see [Getting to know Tasks](https://www.useboomerang.io/docs/boomerang-flow/getting-to-know/tasks).
+  Defining Tasks using this YAML editor is recommended for non-business users who have experience writing yaml definitions and have a desire for further customization in defining a task.
+  ## Creating a Task in YAML
+  The YAML specification has three important sections to be aware of: metadata, params, and steps. 
+  Its important to note that the full Tekton task specification is not yet fully supported. We cannot run multi step tasks, nor do we allow resources to be specified. For more information, see [Known Issues and Limitations](https://www.useboomerang.io/docs/boomerang-flow/introduction/known-issues-limitations)
+  ### Metadata
+  This is the area where we store a series of annotations related to the end user experience and are automatically populated when defining a task view the UI. See below section on understanding the Boomerang Flow specific metadata.
+  ### Params
+  This is where you define Parametes that are needed for the exeuction of the task. These are linked to, or overlayed with, the information for parameters in the annotations.
+  ### Steps
+  Steps are what define the actions a Task will perform. A task must always have atleast one step and is a reference to a container image that executes on specific input and produces output. The order in which the Steps appear in this list is the order in which they will execute.
+  A basic step is made up as follows and is the necessary container contract details to use.
+  \`\`\`
+    steps:
+    - name: Add User To Box Folder
+      image: boomerangio/box-service:0.0.10
+      command: null
+      args:
+      - -props
+      - box
+      - join
+      - folderId
+      - email
+  \`\`\`
+  For more information on steps refer to [Boomerang Flow Tasks](https://www.useboomerang.io/docs/boomerang-flow/3.1.0/getting-to-know/tasks)
+  ## Understanding the available metadata
+  The metadata used by Boomerang Flow is over and above the standard Kubernetes or Tekton metadata and is a way for the information needed for the user experience to be stored.
+  ### Icon
+  You can define the icon that users see in the Workflow Editor by using the following annotation. The available icons can be found [here](path to boomerang flow documentation)
+  \`\`\`
+  boomerang.io/icon: Add
+  \`\`\`
+  ### Params
+  The following definition will match the \`key\` element with the \`name\` element in the params specification of the task. Additional parameter types are supported for the experience including texteditor, boolean, and options. For more detailed information on the available parameters please see [Boomerang Flow Tasks](https://www.useboomerang.io/docs/boomerang-flow/3.1.0/getting-to-know/tasks)
+  \`\`\`
+      boomerang.io/params:
+      - required: true
+        placeholder: ""
+        readOnly: false
+        key: folderId
+        label: Folder Id
+        type: text
+        helperText: Box folder id
+        defaultValue: "MyFolder"
+  \`\`\`
+  `;
