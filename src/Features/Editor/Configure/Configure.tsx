855dc1a02f60962c0005938a81af83c75d0d2929
Isabela Kawabata <isabela.kawabata@ibm.com>
2021-05-26 15:10:30 -0300

    feat: update workflow custom labels text

--- src/Features/Editor/Configure/Configure.tsx
+++ src/Features/Editor/Configure/Configure.tsx
@@ -538,8 +538,8 @@ class Configure extends Component<ConfigureProps, ConfigureState> {
           </div>
           <hr className={styles.delimiter} />
           <div className={styles.labelsContainer}>
-            <h1 className={styles.header}>Custom Kubernetes Labels</h1>
-            <p className={styles.subTitle}>Create custom labels for Kubernetes.</p>
+            <h1 className={styles.header}>Custom Labels</h1>
+            <p className={styles.subTitle}>Create custom labels that will be used at execution time and can be useful in debugging the workflow in Kubernetes.</p>
             <div className={styles.labelsContainer}>
               <div className={styles.tagsContainer}>
                 <FieldArray
