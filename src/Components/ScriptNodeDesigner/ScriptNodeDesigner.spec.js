f36f734c5ae96a8cb040e5215630f05051c7402a
Ben Ruby <benjamin.ruby@ibm.com>
2021-06-21 10:59:18 -0600

    chore: clean up

--- src/Components/ScriptNodeDesigner/ScriptNodeDesigner.spec.js
+++ src/Components/ScriptNodeDesigner/ScriptNodeDesigner.spec.js
@@ -1,5 +1,5 @@
 import React from "react";
-import TemplateTaskNodeDesigner from "./TemplateTaskNodeDesigner";
+import ScriptNodeDesigner from "./ScriptNodeDesigner";
 import WorkflowDagEngine from "Utils/dag/WorkflowDagEngine";
 import { EditorContextRender } from "Utils/testing/context";
 import { revisions } from "ApiServer/fixtures";
@@ -8,11 +8,11 @@ import { WorkflowDagEngineMode } from "Constants";
 const diagramEngine = new WorkflowDagEngine({ dag: revisions[0].dag, mode: WorkflowDagEngineMode.Editor });
 const node = {};
 
-describe("TemplateTaskNodeDesigner --- Snapshot", () => {
-  it("Capturing Snapshot of TemplateTaskNodeDesigner", () => {
+describe("ScriptNodeDesigner --- Snapshot", () => {
+  it("Capturing Snapshot of ScriptNodeDesigner", () => {
     const { baseElement } = rtlContextRouterRender(
       <EditorContextRender>
-        <TemplateTaskNodeDesigner diagramEngine={diagramEngine} node={node} />
+        <ScriptNodeDesigner diagramEngine={diagramEngine} node={node} />
       </EditorContextRender>
     );
     expect(baseElement).toMatchSnapshot();
