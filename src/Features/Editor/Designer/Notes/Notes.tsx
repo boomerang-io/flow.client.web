8964a258cc815982ad36e7b2b2bac5c440f102f8
Lucas Grimauth Evangelista <Lucas.Grimauth@ibm.com>
2021-07-05 14:22:53 -0300

    feat: editor notes

---
+++ src/Features/Editor/Designer/Notes/Notes.tsx
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import cx from "classnames";
+import ReactMde from "react-mde";
+import ReactMarkdown from "react-markdown";
+import { ChevronRight16, RequestQuote24 } from "@carbon/icons-react";
+import styles from "./Notes.module.scss";
+import "react-mde/lib/styles/css/react-mde-all.css";
+import "Styles/markdown.css";
+
+interface NotesProps {
+  markdown?: string;
+  updateNotes: ({ markdown }: { markdown: string }) => void;
+}
+
+const mdeToolbarCommands = [
+  ["header", "bold", "italic", "strikethrough"],
+  ["link", "quote", "code"],
+  ["unordered-list", "ordered-list", "checked-list"],
+];
+
+const mdeTabs = {
+  Preview: "preview",
+  Write: "write",
+};
+
+// window resize values
+const defaultWidth = 640;
+const maxWidth = window.innerWidth - 300;
+const minWidth = 400;
+
+function Notes({ markdown, updateNotes }: NotesProps) {
+  const [selectedTab, setSelectedTab] = useState<any>(mdeTabs.Preview);
+  const [isSidenavOpen, setIsSidenavOpen] = useState(false);
+
+  // controls for window resize
+  const [totalWidth, setTotalWidth] = useState(defaultWidth);
+  let startMousePositionX: any;
+
+  const stopResize = () => {
+    window.removeEventListener("mousemove", resize, false);
+    window.removeEventListener("mouseup", stopResize, false);
+  };
+
+  const resize = (event: any) => {
+    const mousePositionX = event.clientX;
+    const increment = mousePositionX - startMousePositionX;
+    const newWidth = totalWidth - increment;
+    if (increment && newWidth > minWidth && newWidth < maxWidth) {
+      setTotalWidth(newWidth);
+    }
+  };
+
+  const startResize = async (event: any) => {
+    startMousePositionX = event.clientX;
+    window.addEventListener("mousemove", resize, false);
+    window.addEventListener("mouseup", stopResize, false);
+  };
+
+  const resetNotesContainer = () => {
+    setTotalWidth(defaultWidth);
+    setIsSidenavOpen(!isSidenavOpen);
+  };
+
+  const handleChangeMarkdown = (markdown: string) => {
+    updateNotes({ markdown });
+  };
+
+  return (
+    <aside
+      id="notes_container"
+      className={cx(styles.container, { [styles.collapsed]: !isSidenavOpen })}
+      style={{ width: `${totalWidth / 16}rem` }}
+    >
+      <button className={styles.resizerContainer} onMouseDown={isSidenavOpen ? startResize : () => {}}>
+        <button className={styles.collapseButton} onClick={resetNotesContainer}>
+          <ChevronRight16 className={styles.chevron} />
+          <RequestQuote24 className={styles.notesIcon} />
+        </button>
+      </button>
+      <ReactMde
+        value={markdown}
+        onChange={handleChangeMarkdown}
+        selectedTab={selectedTab}
+        onTabChange={setSelectedTab}
+        toolbarCommands={mdeToolbarCommands}
+        classes={{
+          reactMde: styles.reactMde,
+          textArea: styles.reactMdeTextArea,
+          preview: styles.reactMdePreview,
+        }}
+        childProps={{
+          writeButton: {
+            className: cx(styles.tabButton, { [styles.selected]: selectedTab === mdeTabs.Write }),
+          },
+          previewButton: {
+            className: cx(styles.tabButton, { [styles.selected]: selectedTab === mdeTabs.Preview }),
+          },
+        }}
+        generateMarkdownPreview={(markdown) =>
+          Promise.resolve(<ReactMarkdown className="markdown-body" source={markdown} />)
+        }
+      />
+    </aside>
+  );
+}
+
+export default Notes;
