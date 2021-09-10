92e795063b2a073c016e517f3a3deb3776a8a437
Isabela Kawabata <isabela.kawabata@ibm.com>
2021-09-10 11:40:49 -0300

    test configure issue

--- src/Components/CreateWorkflow/CreateWorkflowContent/CreateWorkflowContent.tsx
+++ src/Components/CreateWorkflow/CreateWorkflowContent/CreateWorkflowContent.tsx
@@ -1,3 +1,4 @@
+//@ts-nocheck
 import React, { useState, useEffect, useRef } from "react";
 import classNames from "classnames/bind";
 import {
---
+++ src/Features/Editor/Configure/ConfigureStorage/ConfigureStorage.tsx
@@ -0,0 +1,97 @@
+import React from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { TextInput, ModalFlowForm } from "@boomerang-io/carbon-addons-boomerang-react";
+import { Button, ModalBody, ModalFooter } from "@boomerang-io/carbon-addons-boomerang-react";
+
+type Props = {
+  size: string | number;
+  mountPath: string;
+  handleOnChange: (...values: any) => void;
+  closeModal: (...args: any) => void;
+};
+
+
+export default function ConfigureStorage({size, mountPath, handleOnChange, closeModal}: Props) {
+
+   const handleOnSave = (values: any) => {
+    handleOnChange({enabled: true, size: values.size, mountPath: values.mountPath});
+    closeModal();
+  };
+
+  return (
+    <Formik
+      validateOnMount
+      onSubmit={handleOnSave}
+      initialValues={{
+        size: size || 1,
+        mountPath: mountPath || "",
+      }}
+      validationSchema={Yup.object().shape({
+        size: Yup.number().required(),
+        mountPath: Yup.string(),
+      })}
+    >
+      {(formikProps) => {
+        const {
+          values,
+          touched,
+          errors,
+          handleBlur,
+          handleChange,
+          handleSubmit,
+          isValid,
+        } = formikProps;
+
+        return (
+          <ModalFlowForm onSubmit={handleSubmit}>
+            <ModalBody>
+              <TextInput
+                id="size"
+                data-testid="size"
+                helperText="Size in Gigabytes"
+                invalid={errors.size && touched.size}
+                invalidText={errors.size}
+                labelText={"Storage Size"}
+                min={0}
+                name="size"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                placeholder="Size"
+                style={{ minWidth: "10rem" }}
+                type="number"
+                value={values.size}
+              />
+              <TextInput
+                id="mountPath"
+                data-testid="mountPath"
+                helperText={"Type a Mount path for your storage"}
+                invalid={errors.mountPath && touched.mountPath}
+                invalidText={errors.mountPath}
+                labelText={"Mount path"}
+                name="mountPath"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                placeholder="Mount Path"
+                style={{ minWidth: "10rem" }}
+                type="mountPath"
+                value={values.mountPath}
+              />
+            </ModalBody>
+            <ModalFooter style={{ bottom: "0", position: "absolute", width: "100%" }}>
+              <Button kind="secondary" type="button" onClick={closeModal}>
+                Cancel
+              </Button>
+              <Button
+                disabled={!isValid} //disable if the form is invalid or if there is an error message
+                type="submit"
+              >
+                Save
+              </Button>
+            </ModalFooter>
+          </ModalFlowForm>
+        );
+      }}
+    </Formik>
+  );
+}
