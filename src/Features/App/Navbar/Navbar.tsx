08f722b3f587e0093309a405f18c743a78cfcda3
Isabela Kawabata <isabela_mkb@hotmail.com>
2021-09-22 16:02:34 -0300

    feat: add flow icons with links (#90)
    * fat: add flow icons with links
    
    * chore: bump to latest carbon-addons-boomerang-react at 1.2.5
    
    Co-authored-by: Isabela Kawabata <isabela.kawabata@ibm.com>
    Co-authored-by: Ben Ruby <benjamin.ruby@ibm.com>

--- src/Features/App/Navbar/Navbar.tsx
+++ src/Features/App/Navbar/Navbar.tsx
@@ -115,6 +115,7 @@ export default function NavbarContainer({
         user={userData}
         skipToContentProps={skipToContentProps}
         appName="Flow"
+        isFlowApp
       />
     </>
   );
