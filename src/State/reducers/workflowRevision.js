c85fe79b205497e0c6a8a58c9df82d4336459f9a
Isabela Kawabata <isabela.kawabata@ibm.com>
2021-10-14 16:15:18 -0300

    add check for node config

--- src/State/reducers/workflowRevision.js
+++ src/State/reducers/workflowRevision.js
@@ -34,9 +34,10 @@ export function revisionReducer(state, action) {
     }
     case RevisionActionTypes.UpdateNodeConfigWithResult: {
       const { nodeId, inputs, outputs } = action.data;
-      state.config[nodeId].inputs = Boolean(state.config?.[nodeId]?.inputs)
-        ? { ...state.config[nodeId].inputs, ...inputs }
-        : { ...inputs };
+      if(state.config[nodeId])
+        state.config[nodeId].inputs = Boolean(state.config?.[nodeId]?.inputs)
+          ? { ...state.config[nodeId].inputs, ...inputs }
+          : { ...inputs };
       state.config[nodeId].outputs = outputs;
       state.hasUnsavedUpdates = true;
       return state;
