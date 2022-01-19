8ef4d1f0d4f6d5da3f39835e317a353c885bdf59
Tim Bula <timrbula@gmail.com>
2022-01-18 23:34:16 -0600

    Update ErrorBoundary.tsx

--- src/Components/ErrorBoundary/ErrorBoundary.tsx
+++ src/Components/ErrorBoundary/ErrorBoundary.tsx
@@ -5,10 +5,9 @@ import { ErrorBoundary as ReactErrorBoundary, FallbackProps } from "react-error-
 import { ErrorFullPage } from "@boomerang-io/carbon-addons-boomerang-react";
 
 const ErrorMessage: React.FC<FallbackProps> = (props) => {
-  //const history = useHistory();
   return (
     <Flex mt="10rem" alignItems="center" flexDirection="column" justifyContent="center">
-      <ErrorFullPage statusUrl="https://www.betteruptime.com" />
+      <ErrorFullPage statusUrl="" />
     </Flex>
   );
 };
