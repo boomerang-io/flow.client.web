77494fe223377844db4ed77b4254f743a29212f3
Ming Xia Shi <mingxias@cn.ibm.com>
2023-10-26 09:30:41 +0800

    fix: add securitycontext to add new modal

--- src/Features/ManageTeamTasks/Sidenav/AddTaskTemplate/AddTaskTemplateForm/AddTaskTemplateForm.tsx
+++ src/Features/ManageTeamTasks/Sidenav/AddTaskTemplate/AddTaskTemplateForm/AddTaskTemplateForm.tsx
@@ -103,6 +103,8 @@ function AddTaskTemplateForm({ closeModal, taskTemplates, isLoading, handleAddTa
       script: values.script,
       workingDir: values.workingDir,
       envs: newEnvs,
+      serviceAccountName: values.serviceAccountName,
+      securityContext: values.securityContext,
       config: hasFile && Boolean(values.currentRevision) ? values.currentRevision.config : [],
       results: hasFile && Boolean(values.currentRevision) ? values.currentRevision.results : [],
       changelog: { reason: "" },
@@ -149,6 +151,8 @@ function AddTaskTemplateForm({ closeModal, taskTemplates, isLoading, handleAddTa
           : [];
         setFieldValue("envs", formattedEnvs);
         setFieldValue("workingDir", currentRevision?.workingDir);
+        setFieldValue("serviceAccountName", currentRevision?.serviceAccountName);
+        setFieldValue("securityContext", currentRevision?.securityContext);
         setFieldValue("currentRevision", currentRevision);
         setFieldValue("fileData", fileData);
       }
@@ -169,6 +173,8 @@ function AddTaskTemplateForm({ closeModal, taskTemplates, isLoading, handleAddTa
         command: "",
         script: "",
         workingDir: "",
+        serviceAccountName: "",
+        securityContext: "",
         envs: [],
         fileData: {},
         file: undefined,
@@ -192,6 +198,8 @@ function AddTaskTemplateForm({ closeModal, taskTemplates, isLoading, handleAddTa
         script: Yup.string().nullable(),
         workingDir: Yup.string().nullable(),
         image: Yup.string().nullable(),
+        serviceAccountName: Yup.string().nullable(),
+        securityContext: Yup.string().nullable(),
         file: Yup.mixed().test(
           "fileSize",
           "File is larger than 1MiB",
@@ -381,6 +389,24 @@ function AddTaskTemplateForm({ closeModal, taskTemplates, isLoading, handleAddTa
                 keyLabelText="Environments (optional)"
                 placeholder="Enter env"
                 values={values.envs || []}
+              />
+              <TextInput
+                id="serviceAccountName"
+                invalid={errors.serviceAccountName && touched.serviceAccountName}
+                invalidText={errors.serviceAccountName}
+                labelText="Service Account Name (optional)"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.serviceAccountName}
+              />
+              <TextArea
+                id="securityContext"
+                invalid={errors.securityContext && touched.securityContext}
+                invalidText={errors.securityContext}
+                labelText="Security Context (optional)"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.securityContext}
               />
             </ModalBody>
             <ModalFooter>
